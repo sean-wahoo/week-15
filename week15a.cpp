b47349da8abb790ef4327e4ad944f0388e8585f6
= <=>
2021-11-30 17:35:41 -0500

    forgot to commit more often - basic function put together

--- week15a.cpp
+++ week15a.cpp
@@ -2,6 +2,58 @@
 // CIS 1202 501
 // November 30 2021
 
-int main() {
-    return 0;
+#include <iostream>
+
+using namespace std;
+
+char character(char, int);
+class invalidCharacterException
+{
+};
+class invalidRangeException
+{
+};
+
+char character(char start, int offset)
+{
+    try
+    {
+        if (start <= 90 && start >= 65)
+        {
+            int out = start + offset;
+            if (out <= 90 && out >= 65)
+                return static_cast<char>(out);
+            else
+                throw invalidRangeException();
+        }
+        if (start <= 122 && start >= 97)
+        {
+            int out = start + offset;
+            if (out <= 122 && out >= 97)
+                return static_cast<char>(out);
+            else
+                throw invalidRangeException();
+        }
+        else
+            throw invalidCharacterException();
+    }
+    catch (invalidRangeException)
+    {
+        cout << "Character ends up out of range!" << endl;
+    }
+    catch (invalidCharacterException)
+    {
+        cout << "Given character is not valid!" << endl;
+    }
+}
+
+int main()
+{
+    char c;
+    int i;
+    cout << "input a character\n->";
+    cin >> c;
+    cout << "input a number\n->";
+    cin >> i;
+    cout << character(c, i) << endl;
 }
