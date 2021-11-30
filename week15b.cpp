5433a196058f5c8b25a04bf9bbd83746c7c3c909
= <=>
2021-11-30 17:56:30 -0500

    second assignment

---
+++ week15b.cpp
@@ -0,0 +1,26 @@
+// Sean Reichel
+// CIS 1202 501
+// November 30 2021
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int half(int i)
+{
+    return round((static_cast<float>(i) / 2));
+}
+template <typename T>
+T half(T i)
+{
+    return i / 2;
+}
+
+int main()
+{
+    cout << half((int)2) << endl;
+    cout << half((double)3.8) << endl;
+    cout << half((int)5) << endl;
+    cout << half((float)5.2f) << endl;
+}
